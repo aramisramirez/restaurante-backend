46e87270e56b05174d850bf28e1b529aa6248a1a
aramisramirez <brunomelendez110@gmail.com>
2025-06-14 23:16:38 -0600

    Se adjunto el backup de la base de datos

--- obj/Debug/net9.0/backendrestaurante.AssemblyInfo.cs
+++ obj/Debug/net9.0/backendrestaurante.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backendrestaurante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91fc800caaddd72cf6ad8bc2912980dadba00491")]
 [assembly: System.Reflection.AssemblyProductAttribute("backendrestaurante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backendrestaurante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
